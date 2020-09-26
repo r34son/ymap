7d5f711266b7036b31507accdf17ecdf756a52e2
Yahub <seitasanov.yahub@mail.ru>
2020-09-26 12:20:10 +0300

    Updates

---
+++ precache-manifest.dc4ccbfbeafb0ed2b1305a4bbdc94cf1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "726fc0cef773b59fc207e5da657d572c",
+    "url": "/ymaps/index.html"
+  },
+  {
+    "revision": "19873c8d38a574d9188f",
+    "url": "/ymaps/static/css/main.b689435b.chunk.css"
+  },
+  {
+    "revision": "5fe5173e860003d4a56b",
+    "url": "/ymaps/static/js/2.a0f87f5e.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/ymaps/static/js/2.a0f87f5e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "19873c8d38a574d9188f",
+    "url": "/ymaps/static/js/main.a1d6c0c7.chunk.js"
+  },
+  {
+    "revision": "19c312458a6cc379d9e7",
+    "url": "/ymaps/static/js/runtime-main.1d46940b.js"
+  }
+]);
